23e0b6ed07b85f0842d270976e06a8f614423b11
abhaydasah <abhaydas13@gmail.com>
2021-01-23 13:18:59 +0530

    database

---
+++ app/src/main/java/com/example/vehicledatabase/DatabaseHelper.java
@@ -0,0 +1,70 @@
+package com.example.vehicledatabase;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper  extends SQLiteOpenHelper {
+
+      public static final String DATABASE_NAME ="vehicle.db";
+      public static final String TABLE_NAME ="preset_data_table";
+      public static final String COL_1 ="ID";
+      public static final String COL_2 ="FANSPEED";
+      public static final String COL_3 ="ACSTATE";
+      public static final String COL_4 ="TEMPERATURE";
+
+
+
+
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db=this.getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("create table "+TABLE_NAME +"(ID INTEGER PRIMARY KEY AUTOINCREMENT,FANSPEED INTEGER,ACSTATE TEXT,TEMPERATURE INTEGER)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
+    }
+    public boolean insertData(String FANSPEED,String AC,String TEMPER){
+
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues contentValues=new ContentValues();
+        contentValues.put(COL_2,FANSPEED);
+        contentValues.put(COL_3,AC);
+        contentValues.put(COL_4,TEMPER);
+
+
+
+        long Inserted=db.insert(TABLE_NAME,null,contentValues);
+
+
+        if (Inserted==-1){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    public Cursor getAllData(){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res =db.rawQuery("select * from "+TABLE_NAME,null);
+        return res;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/vehicledatabase/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.vehicledatabase;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ImageButton add,sub;
+    Button uploadButton,viewDataButton;
+    int count=0;
+    String value;
+    EditText ac,temper,fanSpeed,ID;
+    DatabaseHelper myDb;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myDb=new DatabaseHelper(this);
+        add=(ImageButton)findViewById(R.id.Add);
+        sub=(ImageButton)findViewById(R.id.sub);
+        fanSpeed=(EditText)findViewById(R.id.fanspeed);
+        ac=(EditText)findViewById(R.id.ac);
+        temper=(EditText)findViewById(R.id.temper);
+        uploadButton = (Button) findViewById(R.id.upload);
+        viewDataButton = (Button) findViewById(R.id.viewdata);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                value=Integer.toString(count);
+                fanSpeed.setText( value);
+
+            }
+        });
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count--;
+                value=Integer.toString(count);
+                fanSpeed.setText( value);
+
+            }
+        });
+
+
+        AddData();
+        viewAll();
+    }
+
+    public  void AddData() {
+        uploadButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = myDb.insertData(fanSpeed.getText().toString(),
+                                ac.getText().toString(),
+                                temper.getText().toString() );
+                        if(isInserted == true)
+                            Toast.makeText(MainActivity.this,"Data Inserted",Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(MainActivity.this,"Data not Inserted",Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
+
+    public void viewAll(){
+
+        viewDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Cursor res = myDb.getAllData();
+               if(res.getCount()==0){
+
+                   showMessage("Error","No Data");
+                   return;
+               }
+               StringBuffer buffer =new StringBuffer();
+               while (res.moveToNext()){
+                   buffer.append("Id:"+res.getString(0)+"\n");
+                   buffer.append("Fan speed:"+res.getString(1)+"\n");
+                   buffer.append("A/C State:"+res.getString(2)+"\n");
+                   buffer.append("Temperature:"+res.getString(3)+"\n\n");
+
+               }
+
+
+               showMessage("Data",buffer.toString());
+
+            }
+        });
+    }
+
+    public void showMessage(String title,String Message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
+    }
+}
